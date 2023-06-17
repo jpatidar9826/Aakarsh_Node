e95ca10e3fc417a8140ff945493554a8cccde7e8
jpatidar9826 <jpatidar9826@gmail.com>
2023-06-18 00:59:11 +0530

    Cors

--- index.js
+++ index.js
@@ -46,6 +46,17 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://krantieducation.com");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+  next();
+});
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
